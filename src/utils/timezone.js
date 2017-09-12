4604cbe0570ae9d7504a75376c899fee84de48d5
mkoziuk <mkoziuk2@gmail.com>
2017-09-13 00:55:38 +0200

    Added error handling

--- src/utils/timezone.js
+++ src/utils/timezone.js
@@ -2,6 +2,10 @@ import moment from 'moment-timezone';
 
 const timezoneUtils = {
   getTimezoneInfo(timezoneName) {
+    if (!timezoneName || !moment.tz.zone(timezoneName)) {
+      throw Error(`No such timezone: ${timezoneName}`);
+    }
+
     const timezoneObj = moment().tz(timezoneName);
     const timezoneInf = timezoneName.replace('_', ' ').split('/');
 
